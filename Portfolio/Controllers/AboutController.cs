74ecaaaa9724e05ad259eceea9841d842a6b6b6f
Bünyamin Epatay <epatayb@gmail.com>
2026-06-30 20:46:19 +0300

    about controller Update action eklendi

--- Portfolio/Controllers/AboutController.cs
+++ Portfolio/Controllers/AboutController.cs
@@ -32,5 +32,21 @@ namespace Portfolio.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult UpdateAbout(int id)
+        {
+            var about = _context.Abouts.Find(id);
+            return View(about);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateAbout(About about)
+        {
+            _context.Abouts.Update(about);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
