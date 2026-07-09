9f3f10a8c943a2a953c4b99d44642f0f87cb8c0b
Bünyamin Epatay <epatayb@gmail.com>
2026-07-09 16:31:26 +0300

    .

---
+++ Portfolio/ViewComponents/Admin/_AdminHeadViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.ViewComponents.Admin
+{
+    public class _AdminHeadViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/ViewComponents/Admin/_AdminSidebarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.ViewComponents.Admin
+{
+    public class _AdminSidebarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
