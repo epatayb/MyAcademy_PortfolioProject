650984606d2403a1b572d4aeaecdaf2aa667afe7
Bünyamin Epatay <epatayb@gmail.com>
2026-07-09 17:36:50 +0300

    .

---
+++ Portfolio/Controllers/ProjectController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProjectController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var projects = _context.Projects.ToList();
+            return View(projects);
+        }
+    }
+}
