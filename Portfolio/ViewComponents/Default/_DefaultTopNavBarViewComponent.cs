f8adc6f704a5e23085d5193f0ea74f1cd2a4ad19
Bünyamin Epatay <epatayb@gmail.com>
2026-07-17 02:12:28 +0300

    update

---
+++ Portfolio/ViewComponents/Default/_DefaultTopNavBarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.ViewComponents.Default
+{
+    public class _DefaultTopNavBarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
