6c0c07164b2960c766d20c28cfa6bdd39f137165
Bünyamin Epatay <epatayb@gmail.com>
2026-07-17 02:12:28 +0300

    update

---
+++ Portfolio/ViewComponents/Default/_DefaultHeadViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.ViewComponents.Default
+{
+    public class _DefaultHeadViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }    
+    }
+}
