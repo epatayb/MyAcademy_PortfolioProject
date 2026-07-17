bc37d116e6eb1944bcd69baa3aa4b309d5301fae
Bünyamin Epatay <epatayb@gmail.com>
2026-07-17 04:30:27 +0300

    update

---
+++ Portfolio/ViewComponents/Default/_DefaultSkillsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.ViewComponents.Default
+{
+    public class _DefaultSkillsViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public _DefaultSkillsViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var skills = _context.Skills.ToList();
+            return View(skills);
+        }
+    }
+}
