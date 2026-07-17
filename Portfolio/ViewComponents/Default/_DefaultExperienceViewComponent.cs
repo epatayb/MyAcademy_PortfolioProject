26e0153c2c4e5b76127a1116a96e6ad83283eded
Bünyamin Epatay <epatayb@gmail.com>
2026-07-17 05:25:16 +0300

    updated

---
+++ Portfolio/ViewComponents/Default/_DefaultExperienceViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.ViewComponents.Default
+{
+    public class _DefaultExperienceViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public _DefaultExperienceViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var experiences = _context.Experiences.OrderByDescending(x => x.Id).ToList();
+            return View(experiences);
+        }
+    }
+}
