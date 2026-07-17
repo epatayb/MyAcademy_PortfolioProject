a44331b276117a36e4e0ededd81fed133b462c74
Bünyamin Epatay <epatayb@gmail.com>
2026-07-17 05:37:25 +0300

    update

---
+++ Portfolio/ViewComponents/Default/_DefaultEducationViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.ViewComponents.Default
+{
+    public class _DefaultEducationViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public _DefaultEducationViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var educations = _context.Educations.OrderByDescending(x => x.Id).ToList();
+            return View(educations);
+        }
+    }
+}
