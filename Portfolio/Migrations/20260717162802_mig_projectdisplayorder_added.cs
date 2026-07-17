10e3bb851e89947d403cb713f5e99e074c2cab6a
Bünyamin Epatay <epatayb@gmail.com>
2026-07-17 19:34:43 +0300

    project display order added

---
+++ Portfolio/Migrations/20260717162802_mig_projectdisplayorder_added.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_projectdisplayorder_added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "Projects");
+        }
+    }
+}
