8c1b1483d9b73940d37864e193d86fd1ade777be
Bünyamin Epatay <epatayb@gmail.com>
2026-07-18 01:22:40 +0300

    update

---
+++ Portfolio/ViewComponents/Default/_DefaultHeroViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.ViewComponents.Default
+{
+    public class _DefaultHeroViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public _DefaultHeroViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var hero = _context.Banners.FirstOrDefault();
+            return View(hero);
+        }
+    }
+}
