87165d7a2d6ff600a6edd0eb74159bb5d8752cbb
Bünyamin Epatay <epatayb@gmail.com>
2026-07-17 15:15:46 +0300

    updated

---
+++ Portfolio/ViewComponents/Default/_DefaultServicesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.ViewComponents.Default
+{
+    public class _DefaultServicesViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public _DefaultServicesViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var services = _context.Services.ToList();
+            return View(services);
+        }
+    }
+}
