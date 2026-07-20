6732abbda1db0faa74ce4f2acae30045b7fe9036
Bünyamin Epatay <epatayb@gmail.com>
2026-07-20 05:40:36 +0300

    update

---
+++ Portfolio/Migrations/20260720023626_AddIsActiveToTechStack.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActiveToTechStack : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "TechStacks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "TechStacks");
+        }
+    }
+}
