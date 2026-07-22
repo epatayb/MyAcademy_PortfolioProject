a00d65e0870a8929cf0a2e364b6b84b3490c73c5
Bünyamin Epatay <epatayb@gmail.com>
2026-07-23 01:12:22 +0300

    updated

---
+++ Portfolio/Controllers/SkillController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Portfolio.Data.Context;
+using System.Threading.Tasks;
+
+namespace Portfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SkillController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var skills = await _context.Skills
+                .AsNoTracking()
+                .Where(x=>x.IsActive)
+                .OrderBy(x=>x.Name)
+                .ToListAsync();
+
+            return View(skills);
+        }
+    }
+}
