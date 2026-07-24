4b3426f82b7fdc3483667a6dd70ef54493d4fc5c
Bünyamin Epatay <epatayb@gmail.com>
2026-07-24 18:03:00 +0300

    updated

--- Portfolio/Data/Entities/Admin.cs
+++ Portfolio/Data/Entities/Admin.cs
@@ -1,4 +1,6 @@
-﻿namespace Portfolio.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Data.Entities
 {
     public class Admin
     {
@@ -7,6 +9,11 @@
         public string FullName { get; set; }
         public string? ImageUrl { get; set; }
         public string UserName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
         public string Password { get; set; }
+        public string? PasswordResetTokenHash { get; set; }
+        public DateTime? PasswordResetTokenExpiresAt { get; set; }
     }
 }
---
+++ Portfolio/Migrations/20260724135143_AddAdminEmailAndPasswordResetFields.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminEmailAndPasswordResetFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetTokenExpiresAt",
+                table: "Admins",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetTokenHash",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetTokenExpiresAt",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetTokenHash",
+                table: "Admins");
+        }
+    }
+}
